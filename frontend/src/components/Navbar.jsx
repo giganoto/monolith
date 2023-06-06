6375752d486d17486f1de11515d72cdf7751ef2a
RitwikKapoor <kapoorritwik2020@gmail.com>
2023-06-06 21:00:13 +0530

    frontend folders added

---
+++ frontend/src/components/Navbar.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div className='flex'>
+          <div className="logo" onClick={() => navigate("/")} >
+              <h1>GIGANOTO</h1>
+          </div>
+        <div className='flex'>
+           
+        </div>
+    </div>
+  )
+}
+
+export default Navbar
