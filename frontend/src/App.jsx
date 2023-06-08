14f6d10c200ccd0219844949d34c7c18b8a9055f
RitwikKapoor <kapoorritwik2020@gmail.com>
2023-06-08 18:36:24 +0530

    navbar added

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,17 +3,17 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Blogs from "./pages/Blogs/Blogs";
 import DSA from "./pages/DSA/DSA";
 import PageNotFound from "./pages/PageNotFound/PageNotFound";
-import Navbar from "./components/Navbar";
 import Home from "./pages/Home/Home";
+import Navbar from "./components/Navbar/Navbar";
 
 
 function App(){
     return (
         <BrowserRouter>
-            <Navbar/>   
+            <Navbar/>
             <Routes>
                 <Route path = "/" element={<Home/>} />
-                <Route path = "/blogs/:id" element={<Blogs/>} />
+                <Route path = "/blogs" element={<Blogs/>} />
                 <Route path="/dsa-sheet" element={<DSA/>} />
                 <Route path="*"  element={<PageNotFound/>} />
             </Routes>
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -5,7 +5,13 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        gigaRed: '#D90B28',
+        btnOrange: '#FF4820',
+      },
+    },
   },
   plugins: [],
 }
+
