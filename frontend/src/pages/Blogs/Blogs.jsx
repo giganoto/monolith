c403d8d36f3dee7b4f8b2691708edff2ae0326b1
Somnath Golui <code.somu@gmail.com>
2023-06-19 15:27:42 +0530

    Made the blogs page responsive for all screens

--- frontend/src/pages/Blogs/Blogs.jsx
+++ frontend/src/pages/Blogs/Blogs.jsx
@@ -2,25 +2,38 @@ import { useState } from "react";
 import BlogsCard from "./BlogCard";
 import InfiniteScroll from "react-infinite-scroll-component";
 
+let contentLength = 3;
+
+if (window.innerWidth > 768 && window.innerWidth < 1125) {
+  contentLength = 6;
+}
+
+if (window.innerWidth >= 1125) {
+  contentLength = 9;
+}
+
 const Blogs = () => {
-  const [dataSource, setDataSource] = useState(Array.from({ length: 9 }));
+  const [dataSource, setDataSource] = useState(
+    Array.from({ length: contentLength })
+  );
   const [hasMore, setHashMore] = useState(true);
 
   const fetchMoreData = () => {
     setTimeout(() => {
-      setDataSource(dataSource.concat(Array.from({ length: 9 })));
-    }, 1000);
+      setDataSource(dataSource.concat(Array.from({ length: contentLength })));
+    }, 2000);
   };
   return (
     <>
       <div className="max-w-[1216px] m-auto text-white">
-        <h2 className="mb-4 text-xl font-bold">Latest Post</h2>
+        <h2 className="mb-4 ml-6 text-xl font-bold">Latest Post</h2>
         <InfiniteScroll
           dataLength={dataSource.length}
           next={fetchMoreData}
           hasMore={hasMore}
-          loader={<h4 className="flex-nowrap">Loading...</h4>}
-          className="flex flex-wrap place-content-between"
+          loader={<h4 className="absolute bottom-2">Loading...</h4>}
+          // className="flex flex-wrap place-content-between"
+          className="flex flex-col items-center md:flex-row md:flex-wrap md:justify-evenly relative"
         >
           {dataSource.map((item, index) => {
             return <BlogsCard key={index} />;
