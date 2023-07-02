9665ba28c7cc86352aeab950f34cad28b91e2ed6
siddhant ota <tothesip@gmail.com>
2023-07-02 12:13:42 +0530

    image upload feature added

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -3,11 +3,21 @@ from flask_sqlalchemy import SQLAlchemy
 import os
 from app.blog.blog import blog
 from app.users.users import users
+import boto3
+from botocore.client import Config
 
 app = Flask(__name__)
-app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get('DATABASE_URI')
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DATABASE_URI")
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+
 db = SQLAlchemy(app)
+s3_client = boto3.resource(
+    "s3",
+    endpoint_url="https://cea4f9e4f759ff633f4e453ea9aabb85.r2.cloudflarestorage.com",
+    aws_access_key_id=os.environ.get("AWS_ACCESS_KEY_ID"),
+    aws_secret_access_key=os.environ.get("AWS_SECRET_ACCESS_KEY"),
+    config=Config(signature_version="s3v4"),
+)
 
 app.register_blueprint(blueprint=blog)
 app.register_blueprint(blueprint=users)
--- backend/run.py
+++ backend/run.py
@@ -1,9 +1,11 @@
 from app import app, db
 
 if __name__ == '__main__':
+    from app import s3_client
     from app.database.models import Users, Posts
 
     with app.app_context():
+        print(s3_client)
         db.create_all()
 
     app.run(debug=True, host="0.0.0.0")
