97699af8fd380c3ddcea5fb54ceb5fb4dcf57531
Siddhant Ota <uncleslayer@pop-os.localdomain>
2023-07-12 16:46:18 +0530

    authentication and admin authorization in create blog api

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -5,6 +5,9 @@ from app.blog.blog import blog
 from app.users.users import users
 import boto3
 from botocore.client import Config
+import firebase_admin
+from firebase_admin import credentials
+
 
 app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DATABASE_URI")
@@ -13,11 +16,14 @@ app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 db = SQLAlchemy(app)
 s3_client = boto3.resource(
     "s3",
-    endpoint_url="https://cea4f9e4f759ff633f4e453ea9aabb85.r2.cloudflarestorage.com",
+    endpoint_url=f"https://{os.environ.get('CLOUDFLARE_ACCOUNT_ID')}.r2.cloudflarestorage.com",
     aws_access_key_id=os.environ.get("AWS_ACCESS_KEY_ID"),
     aws_secret_access_key=os.environ.get("AWS_SECRET_ACCESS_KEY"),
     config=Config(signature_version="s3v4"),
 )
+
+firebase_cred = credentials.Certificate("app/firebase.json")
+firebase_app = firebase_admin.initialize_app(firebase_cred)
 
 app.register_blueprint(blueprint=blog)
 app.register_blueprint(blueprint=users)
--- backend/run.py
+++ backend/run.py
@@ -3,9 +3,11 @@ from app import app, db
 if __name__ == '__main__':
     from app import s3_client
     from app.database.models import Users, Posts
+    from app import firebase_app
 
     with app.app_context():
         print(s3_client)
+        print(firebase_app)
         db.create_all()
 
     app.run(debug=True, host="0.0.0.0")
