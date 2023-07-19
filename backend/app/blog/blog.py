8a5995665210e6af4733995124d487a87600dbf3
siddhant ota <siddhantota@protonmail.com>
2023-07-20 00:03:04 +0530

    admin check in api level added

--- backend/app/blog/blog.py
+++ backend/app/blog/blog.py
@@ -13,52 +13,55 @@ def create_post():
     import time
 
     auth_token = request.cookies.get("access_token")
-    print('and the token is: ==>')
-    print(auth_token)
-
-    return {'message': "hi"}
-
     decrypted_token_data = (verify_id_token(auth_token))
-    print(decrypted_token_data)
 
     blog_info = json.loads(request.form.get("blogData"))
     blog_image = request.files.getlist("blogImage")
 
     blog_author = Users.query.filter_by(email=decrypted_token_data["email"]).first()
-    print(blog_author)
 
-    if blog_author.email in admins:
-        
-        for index, image in enumerate(blog_image):
-            import io
-            file_like = io.BytesIO(image.read())
-            s3_client.Bucket("giganoto").upload_fileobj(file_like, f'{image.filename}-{index}')
-            url = "need to configured according to the domain name"
-
-        blog_post = Posts(
-            author = blog_author.id,
-            title = blog_info["postTitle"],
-            content = blog_info["postContent"],
-            post_time = str(time.time()),
-            blog_url = url
-        )
-        
-        db.session.add(blog_post)
-        db.session.commit()
-
+    if blog_author.email not in admins:
         return {
-            "message": "success"
-        }, 201
+            "message": "not authorized to create blog"
+        }, 403
+
+    for index, image in enumerate(blog_image):
+        import io
+        file_like = io.BytesIO(image.read())
+        s3_client.Bucket("giganoto").upload_fileobj(file_like, f'{image.filename}-{index}')
+        url = "need to configured according to the domain name"
+
+    blog_post = Posts(
+        author = blog_author.id,
+        title = blog_info["postTitle"],
+        content = blog_info["postContent"],
+        post_time = str(time.time()),
+        blog_url = url
+    )
+        
+    db.session.add(blog_post)
+    db.session.commit()
 
     return {
-        "message": "not authorized to create blog"
-    }, 403
+        "message": "success"
+    }, 201
+
 
 # ADMIN ONLY
 @blog.route("/blog/update-post", methods=["POST"])
 def update_post():
     
-    from app.database.models import Posts, db
+    from app.database.models import Posts, db, Users
+    
+    access_token = request.cookies.get("access_token")
+    access_token_decrypted = verify_id_token(blog_author)
+
+    blog_author = Users.query.filter_by(email=access_token_decrypted["email"]).first()
+
+    if blog_author.email not in admins:
+        return {
+            "error": "not authorized to update blog"
+        }
 
     post_to_update_info = request.get_json()
 
@@ -78,6 +81,16 @@ def update_post():
 @blog.route("/blog/delete-post", methods=["DELETE"])
 def delete_post():
     from app.database.models import Posts, db
+
+    access_token = request.cookies.get("access_token")
+    access_token_decrypted = verify_id_token(blog_author)
+
+    blog_author = Users.query.filter_by(email=access_token_decrypted["email"]).first()
+
+    if blog_author.email not in admins:
+        return {
+            "error": "not authorized to update blog"
+        }
 
     post_to_delete_info = request.get_json()
 
